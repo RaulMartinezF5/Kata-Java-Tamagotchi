907d4f13c6e2762168de6bc81b5b68b9e415bca7
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-02-01 09:57:06 +0100

    en proceso

--- src/main/java/com/kata/Tamagotchi.java
+++ src/main/java/com/kata/Tamagotchi.java
@@ -6,4 +6,9 @@ public class Tamagotchi {
     int hunger = 4;
     int energy = 4;
 
+    public int getMood() {return mood;}
+
+    public int getHunger() {return hunger;}
+
+    public int getEnergy() {return energy;}
 }
